e7288e353ed4351dcf7a420beb9bcab6e42f25e4
kanzhamada <hamadakanz@gmail.com>
2025-11-03 14:34:41 +0700

    service admin page

---
+++ src/routes/admin/Service/Create/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageData, ActionData } from './$types.js';
+	import ServiceForm from '$lib/components/Admin/AdminForm/ServiceForm.svelte';
+	let { data, form }: { data: PageData; form?: ActionData } = $props();
+</script>
+
+<ServiceForm
+	{data}
+	title="Create New Service"
+	action="create"
+	success={form?.success ?? false}
+	message={form?.message ?? ''}
+/>
+
+
---
+++ src/routes/admin/Service/[id]/Edit/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { PageData, ActionData } from './$types.js';
+	import { page } from '$app/state';
+	import ServiceForm from '$lib/components/Admin/AdminForm/ServiceForm.svelte';
+	import { getServiceById } from '$lib/api/admin/service.js';
+
+	let { data, form }: { data: PageData; form?: ActionData } = $props();
+
+	const { id } = page.params;
+
+	const response = getServiceById(id);
+</script>
+
+<ServiceForm
+	{data}
+	detailData={response.data}
+	action="edit"
+	title="Edit Service"
+	success={form?.success ?? false}
+	message={form?.message ?? ''}
+/>
